dd37cb664f7d5fd4a9f97b40e2c7accac8dff155
Vinícius Delmo <124913845+vinicius-delmo@users.noreply.github.com>
2023-05-16 13:05:02 -0300

    hmtl-css rethink

---
+++ reactFundamentos/reactapp/src/components/Card/index.jsx
@@ -0,0 +1,9 @@
+import './styles.css';
+export function Card(props) {
+  return (
+    <div className="card">
+      <strong>{props.name}</strong>
+      <small>{props.time}</small>
+    </div>
+  );
+}
